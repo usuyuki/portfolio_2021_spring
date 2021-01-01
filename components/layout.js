2e3e0ab646c5b3244a1456ef407176716a9f0008
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2021-01-01 17:59:49 +0900

    wip チュートリアルをパーソナライズ中

--- components/layout.js
+++ components/layout.js
@@ -3,8 +3,8 @@ import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
-const name = "Your Name";
-export const siteTitle = "Next.js Sample Website";
+const name = "うすゆき";
+export const siteTitle = "うすゆきポートフォリオwithNext.js";
 
 export default function Layout({ children, home }) {
   return (
