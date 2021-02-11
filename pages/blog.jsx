e1c0bb3b82ddb587862e5aaf7871f5615cb15689
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2021-02-11 18:40:15 +0900

    feat noteの取得

--- pages/blog.jsx
+++ pages/blog.jsx
@@ -70,6 +70,54 @@ export default function Home({ setNote, setWPblog }) {
         </article>
 
         <h2 className="my-10 mx-auto text-center font-bold text-2xl">note</h2>
+        <article className="p-5 mx-10 border-green-500 border-2">
+          {/* ここからブログ */}
+
+          <h2 className="text-center text-2xl mt-5">Latest Blogs</h2>
+          <ul className="">
+            <li className="m-5" key={setNote[0].id}>
+              <Link href={`/blogs/${setNote[0].id}`}>
+                <a>{setNote[0].title}</a>
+              </Link>
+              <br />
+              <small className="text-center text-gray-600">
+                {setNote[0].date}
+              </small>
+
+              <p>{setNote[0].content}</p>
+              <p>{setNote[0].work_tech}</p>
+            </li>
+            <li className="m-5" key={setNote[1].id}>
+              <Link href={`/blogs/${setNote[1].id}`}>
+                <a>{setNote[1].title}</a>
+              </Link>
+              <br />
+              <small className="text-center text-gray-600">
+                {setNote[1].date}
+              </small>
+
+              <p>{setNote[1].content}</p>
+              <p>{setNote[1].work_tech}</p>
+            </li>
+            <li className="m-5" key={setNote[1].id}>
+              <Link href={`/blogs/${setNote[1].id}`}>
+                <a>{setNote[1].title}</a>
+              </Link>
+              <br />
+              <small className="text-center text-gray-600">
+                {setNote[1].date}
+              </small>
+
+              <p>{setNote[1].content}</p>
+              <p>{setNote[1].work_tech}</p>
+            </li>
+          </ul>
+          <button className="mx-5 text-center rounded border border-4 text-blue-800 border-purple-700 py-1 px-2 mb-5 hover:bg-purple-700 hover:text-white animation-ping duration-1000">
+            <Link href="/archive/allBlogs">
+              <a className="hover:text-white">Show more</a>
+            </Link>
+          </button>
+        </article>
         <h2 className="my-10 mx-auto text-center font-bold text-2xl">Qiita</h2>
         <h2 className="my-10 mx-auto text-center font-bold text-2xl">Zenn</h2>
       </main>
