92c7c44031a28344118f1f2682ee08328d3a0ce1
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2021-03-28 16:10:05 +0900

    fix

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,9 +1,9 @@
 export default function Custom404() {
   return (
     <div className="text-center mt-40">
-      <div class="bird"></div>
-      <div class="bird -type_2"></div>
-      <div class="bird -type_3"></div>
+      <div className="bird"></div>
+      <div className="bird -type_2"></div>
+      <div className="bird -type_3"></div>
       <h1>404 - Page Not Found</h1>
       <h3>Would you like to some more coffee?</h3>
     </div>
